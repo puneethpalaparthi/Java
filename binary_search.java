9c412536f9080bbb703e5286e9d87f9332676cd3
Puneeth <160226241+puneethpalaparthi@users.noreply.github.com>
2024-09-12 12:03:19 +0530

    Update binary_search.java

--- binary_search.java
+++ binary_search.java
@@ -1,77 +1,114 @@
-import java.util.Scanner;
+import java.util.*;
+public class Binary
+{
 
-public class Binary_search {
-
-    static void readarr(int ar[], int n) {
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < n; i++) {
-            ar[i] = sc.nextInt();
-        }
-    }
-
-    static void printarr(int ar[], int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.printf("%d\t", ar[i]);
-        }
-        System.out.println();
-    }
-
-    public static void swap(int[] a, int[] b) {
-        int temp = a[0];
-        a[0] = b[0];
-        b[0] = temp;
-    }
-
-    public static void sort(int ar[], int n) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (ar[i] > ar[j]) {
-                    int[] a = {ar[i]};
-                    int[] b = {ar[j]};
-                    swap(a, b);
-                    ar[i] = a[0];
-                    ar[j] = b[0];
-                }
-            }
-        }
-    }
-
-    public static void search(int ar[], int n, int key) {
-        int start = 0, end = n - 1, mid;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the key to find the position:");
-        key = sc.nextInt();
-        
-        while (start <= end) {
-            mid = (start + end) / 2;
-            if (key == ar[mid]) {
-                System.out.printf("The key %d is found at the position=%d%n", key, mid + 1);
-                return;
-            } else if (key < ar[mid]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        System.out.println("The key is not found");
-    }
-
-    public static void main(String[] args) {
-        int n, key;
-        int[] arr = new int[100];
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Enter the number of elements:");
-        n = sc.nextInt();
-        
-        System.out.println("Enter the elements of the array:");
-        readarr(arr, n);
-        
-        sort(arr, n);
-        
-        System.out.println("The array after sorting:");
-        printarr(arr, n);
-        
-        search(arr, n, 0);
-    }
-}
+	static void readar(int a[],int n)
+	{	
+		Scanner sc=new Scanner(System.in);
+		
+		for(int i=0;i<n;i++)
+		{	
+			a[i]=sc.nextInt();
+		}
+	}
+	static void printar(int a[],int n)
+	{
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(a[i]);
+		}
+	} 
+	
+	
+	static void sort(int a[],int n)
+	{
+		for(int i=0;i<n-1;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+	}
+	static void search(int a[],int n)
+	{
+		int key,start=0,mid,end=n-1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the key to search : ");
+		key=sc.nextInt();
+		boolean found = false;
+		while(start<end)
+		{
+			mid=(start+end)/2;
+			if(key==a[mid])
+			{
+				System.out.println("the  key is found at index "+mid);
+				 found =true;
+				break;
+			}
+			else if(key<a[mid])
+			{
+				end=mid-1;
+			}
+			else
+			{
+				start=mid+1;
+			}
+		}
+		if(found!=true)
+		System.out.println("the key not found!");
+	}
+	
+	public static void main(String args[])
+	{
+		int n,key;
+		int[] a = new int[100];
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter n:");
+		n=sc.nextInt();
+		System.out.println("enter the elements:");	
+		readar(a,n);
+		
+		sort(a,n);
+		System.out.println("the array after sorting:");
+		printar(a,n);
+		
+		search(a,n);
+	}
+}	
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		
+	
+	
